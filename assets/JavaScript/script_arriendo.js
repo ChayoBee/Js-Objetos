5582692d64265bbe2d58095dca99344888fb8ee3
ChayoBee <cacao34@hotmail.com>
2023-12-08 01:35:41 -0300

    Commit 1

---
+++ assets/JavaScript/script_arriendo.js
@@ -0,0 +1,12 @@
+const propiedades_alquiler = [
+    {
+    nombre: '',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    costo: 0,
+    smoke: false,
+    pets: true
+    }
+    ]
---
+++ assets/JavaScript/script_venta.js
@@ -0,0 +1,12 @@
+const propiedades_venta = [
+    {
+    nombre: '',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    costo: 0,
+    smoke: false,
+    pets: true
+    }
+    ]
